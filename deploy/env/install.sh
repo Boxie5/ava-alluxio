b7999265134f988bf1c5e714be97acbce9e96f4b
Bowen Xie <boxie5@126.com>
2018-10-26 00:17:45 +0800

    install docker script

--- deploy/env/install.sh
+++ deploy/env/install.sh
@@ -13,50 +13,41 @@ apt-get update
 apt-get install -y git ca-certificates curl
 
 # ===============================================================
-# install ansible
-# ---------------------------------------------------------------
-
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt update
-sudo apt-get install -y ansible
-
-# ===============================================================
 # install docker
 # ---------------------------------------------------------------
-
 # remove bad route if it exist
+
 if ip route list | grep -q "172.16.0.0/12 via 192.168.212.254 dev bond0"; then
   ip route del 172.16.0.0/12 via 192.168.212.254
 fi
 
-sudo apt-get install -y apt-transport-https
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
+apt-get install -y apt-transport-https
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
 
 # ===============================================================
 # docker settings
 # ---------------------------------------------------------------
 
 if ! grep -q docker /etc/group; then
-  sudo groupadd docker
+  groupadd docker
 fi
-sudo usermod -aG docker $USER
+usermod -aG docker $USER
 mkdir -p /home/"$USER"/.docker
-sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
-sudo chmod g+rwx "/home/$USER/.docker" -R
+chown "$USER":"$USER" /home/"$USER"/.docker -R
+chmod g+rwx "/home/$USER/.docker" -R
 
-sudo service docker stop
+service docker stop
 mkdir -p -m 777 $DATA_DISK/docker
 
 tar -zcC /var/lib docker > $DATA_DISK/var_lib_docker-backup-$(date +%s).tar.gz
-sudo mv /var/lib/docker $DATA_DISK/docker
+mv /var/lib/docker $DATA_DISK/docker
 ln -s $DATA_DISK/docker /var/lib/docker
-sudo service docker start
+service docker start
 
-sudo systemctl enable docker
+systemctl enable docker
 
 # ===============================================================
 # pull basic docker images
